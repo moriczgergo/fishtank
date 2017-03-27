aa5230f35bffd0e442b996390215e2a86dc4629b
Gergő Móricz <mo.geryy@gmail.com>
2017-03-27 12:32:35 +0200

    Added coin system, and !money command.

--- index.js
+++ index.js
@@ -1,11 +1,86 @@
 var PlugAPI = require('plugapi');
 var creds = require('./creds.js');
+var fs = require('fs');
+
+const coinPerPlay = 1;
+const list = 10137463;
+
 var bot = new PlugAPI({
     email: creds.email,
     password: creds.password
 });
 
-var list = 10137463;
+function fileChecks(){
+    var fileUnparsed = "";
+    try {
+        fileUnparsed = fs.readFileSync("usr.json").toString();
+        var fileContents = JSON.parse(fs.readFileSync("usr.json").toString());
+    } catch(e) {
+        console.log("[fileCheckError] " + fileUnparsed + ":" + e);
+        var fileContents = { "users": [] };
+    }
+    return fileContents;
+}
+
+function updateFile(contents){
+    if (contents != null && typeof contents != "undefined" && typeof contents != "null" && contents != "null"){
+        console.log("[fileUpdate] " + contents);
+        fs.truncateSync("usr.json");
+        fs.writeFileSync("usr.json", contents, {"encoding": "utf8"});
+    } else {
+        console.log("[fileUpdate] contents is " + contents);
+    }
+}
+
+function newUser(user){
+    var usrindex = -1;
+    var fileContents = fileChecks();
+    fileContents.users.forEach(function(filevalue, fileindex){
+        if (user.username == filevalue.username){
+            usrindex = fileindex;
+        }
+    });
+    if (usrindex == -1){
+        fileContents.users.push({ "username": user.username, "coins": 0 });
+        return fileContents;
+    } else {
+        return null;
+    }
+}
+
+function addCoin(user, amount){
+    var fileContents = fileChecks();
+    var usrindex = -1;
+    fileContents.users.forEach(function(value,index){
+        if (value.username == user){
+            usrindex = index;
+        }
+    });
+    if (usrindex != -1){
+        fileContents.users[usrindex].coins += amount;
+        return fileContents;
+    } else {
+        console.log("[addCoin] user " + user + " doesn't exist");
+        return null;
+    }
+}
+
+function getCoin(user){
+    var fileContents = fileChecks();
+    var coins = -1;
+    fileContents.users.forEach(function(value,index){
+        if (value.username == user){
+            coins = value.coins;
+        }
+    });
+    if (coins != -1){
+        console.log("[getCoin] " + user + " has " + coins + " coins.");
+        return coins;
+    } else {
+        console.log("[getCoin] " + user + " is not found.");
+        return 0;
+    }
+}
 
 bot.on('roomJoin', function(room) {
     console.log("Joined " + room + ".");
@@ -22,10 +97,21 @@ bot.on('roomJoin', function(room) {
             bot.leaveBooth();
         }
     }
+    bot.getUsers().forEach(function(value, index){
+        updateFile(JSON.stringify(newUser(value)));
+    });
+});
+
+bot.on('userJoin', function(data){
+    updateFile(JSON.stringify(newUser(data)));
 });
 
 bot.on('command:hello', function(data){
     bot.sendChat("Hey, @" + data.from.username + "! I am " + bot.getSelf().username + ", a little decoration in this room! I also play music when noone else is playing. Ask me anything.");
+});
+
+bot.on('command:money', function(data){
+    bot.sendChat("@" + data.from.username + " You have " + getCoin(data.from.username) + " coins.");
 });
 
 bot.on('advance', function(data){
@@ -40,6 +126,10 @@ bot.on('advance', function(data){
         if (bot.getDJ().id != bot.getSelf().id){
             bot.leaveBooth();
         }
+    }
+    if (bot.getDJ() != null){
+        var dj = bot.getDJ();
+        updateFile(JSON.stringify(addCoin(dj.username, coinPerPlay)));
     }
 });
 
